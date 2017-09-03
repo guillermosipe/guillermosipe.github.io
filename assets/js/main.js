a3ed462f74a824ea027006d0464ccf566035081c
Guillermo Silva Peña <guillermo.sipe@gmail.com>
2017-09-03 18:46:35 -0500

    Agregar informacion sobre lenguajes y modificacion de formato

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,6 @@ jQuery(document).ready(function($) {
         
         }
     );
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
 
 
 });
